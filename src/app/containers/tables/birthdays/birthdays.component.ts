431f276cc8333f2fc86c824e900ba308a7056a69
lisandroprada <lisandro.prada@gmail.com>
2023-02-01 11:27:58 -0300

    Fix auto hide columns

--- src/app/containers/tables/birthdays/birthdays.component.ts
+++ src/app/containers/tables/birthdays/birthdays.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { CustomerData } from 'src/app/interfaces/ICustomer';
 import { CustomerService } from 'src/app/services/customer.service';
+import { MenuService } from 'src/app/services/menu.service';
 
 @Component({
   selector: 'app-birthdays',
@@ -23,27 +24,35 @@ import { CustomerService } from 'src/app/services/customer.service';
   ],
 })
 export class BirthdaysComponent implements OnInit {
-  displayedColumns: string[] = [
-    // 'customer_ID',
-    'name',
-    // 'ci',
-    'birthday',
-    'phone',
-    'status',
-    // 'since',
-  ];
+  columns: string[] = ['name', 'birthday'];
+  columnsExt: string[] = ['name', 'birthday', 'phone', 'status'];
+
+  displayedColumns: string[] = this.columnsExt;
 
   dataSource: MatTableDataSource<CustomerData>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-  constructor(private customerService: CustomerService) {
+
+  constructor(
+    private customerService: CustomerService,
+    private menuService: MenuService
+  ) {
     this.customerService.getStatistics().subscribe((data: any) => {
       console.log(data);
       this.dataSource = new MatTableDataSource(data.actualBirthDays);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
     });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (window.innerWidth <= 750) {
+      this.displayedColumns = this.columns;
+    } else {
+      this.displayedColumns = this.columnsExt;
+    }
   }
 
   ngOnInit(): void {}
